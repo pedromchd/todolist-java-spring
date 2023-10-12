b4e469774ec92ef13d623f307fe1af500e0bba3c
pedromchd <pedrogmachado28@outlook.com>
2023-10-12 17:36:44 -0300

    user crud implementation

--- src/main/java/br/com/pedromchd/todolist/controllers/UserController.java
+++ src/main/java/br/com/pedromchd/todolist/controllers/UserController.java
@@ -1,19 +1,62 @@
 package br.com.pedromchd.todolist.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pedromchd.todolist.dtos.UserDTO;
 import br.com.pedromchd.todolist.models.User;
+import br.com.pedromchd.todolist.services.interfaces.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody User user) {
-        System.out.println(user.getUsername());
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.getAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Integer id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody UserDTO userDTO) {
+        User user = userService.save(userDTO);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        User user = userService.update(id, userDTO);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Integer id) {
+        Boolean deleted = userService.delete(id);
+        if (deleted) {
+            return new ResponseEntity<Boolean>(deleted, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Boolean>(deleted, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
---
+++ src/main/java/br/com/pedromchd/todolist/services/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package br.com.pedromchd.todolist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.pedromchd.todolist.dtos.UserDTO;
+import br.com.pedromchd.todolist.models.User;
+import br.com.pedromchd.todolist.repositories.UserRepository;
+import br.com.pedromchd.todolist.services.interfaces.UserService;
+
+public class UserServiceImpl implements UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getById(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public User save(UserDTO userDTO) {
+        User user = new User();
+        user.setUsername(userDTO.username());
+        user.setFullname(userDTO.fullname());
+        user.setPassword(userDTO.password());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(Integer id, UserDTO userDTO) {
+        User user = getById(id);
+        user.setUsername(userDTO.username());
+        user.setFullname(userDTO.fullname());
+        user.setPassword(userDTO.password());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
