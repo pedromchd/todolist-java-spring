3c621ceac5b9731d6b83e99b1b000521a8c6c233
pedromchd <pedrogmachado28@outlook.com>
2023-10-12 10:29:50 -0300

    lombok @data

--- src/main/java/br/com/pedromchd/todolist/models/User.java
+++ src/main/java/br/com/pedromchd/todolist/models/User.java
@@ -1,5 +1,8 @@
 package br.com.pedromchd.todolist.models;
 
+import lombok.Data;
+
+@Data
 public class User {
     
     private String username;
@@ -7,29 +10,5 @@ public class User {
     private String fullname;
 
     private String password;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
 }
