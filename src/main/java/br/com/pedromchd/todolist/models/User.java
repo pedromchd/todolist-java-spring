3a4279a214fa02a8f9e826b1a1bcf607ce6b3ef8
pedromchd <pedrogmachado28@outlook.com>
2023-10-12 12:52:20 -0300

    user model

--- src/main/java/br/com/pedromchd/todolist/models/User.java
+++ src/main/java/br/com/pedromchd/todolist/models/User.java
@@ -1,14 +1,39 @@
 package br.com.pedromchd.todolist.models;
 
-import lombok.Data;
+import java.time.LocalDateTime;
 
-@Data
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
 public class User {
     
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
     private String username;
-
+    
+    @Column(length = 100, nullable = false)
     private String fullname;
-
+    
+    @Column(length = 255, nullable = false)
     private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
 }
