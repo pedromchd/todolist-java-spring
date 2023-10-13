37d2ea0c9859de5b794ec7bb3555b5cf23090b3f
pedromchd <pedrogmachado28@outlook.com>
2023-10-13 15:44:22 -0300

    adding annotations

--- src/main/java/br/com/pedromchd/todolist/models/User.java
+++ src/main/java/br/com/pedromchd/todolist/models/User.java
@@ -11,12 +11,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "user")
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class User {
     
     @Id
@@ -33,6 +37,7 @@ public class User {
     @Column(length = 255, nullable = false)
     private String password;
 
+    @Column
     @CreationTimestamp
     private LocalDateTime createdAt;
     
