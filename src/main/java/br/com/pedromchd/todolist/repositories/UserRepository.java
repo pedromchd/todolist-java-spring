50fa0c7f871296cbc26957868fa86002d6eb86aa
pedromchd <pedrogmachado28@outlook.com>
2023-10-13 16:00:47 -0300

    repositories

--- src/main/java/br/com/pedromchd/todolist/repositories/UserRepository.java
+++ src/main/java/br/com/pedromchd/todolist/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package br.com.pedromchd.todolist.repository;
+package br.com.pedromchd.todolist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
