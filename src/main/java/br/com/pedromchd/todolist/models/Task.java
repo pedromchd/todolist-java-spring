acb0874fd7d5814b6c4b632e1a07f2a1c0c32f68
pedromchd <pedrogmachado28@outlook.com>
2023-10-13 15:43:59 -0300

    tasks model

---
+++ src/main/java/br/com/pedromchd/todolist/models/Task.java
@@ -0,0 +1,64 @@
+package br.com.pedromchd.todolist.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Task {
+    
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+    
+    @Column(length = 255)
+    private String description;
+    
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+    
+    @Column
+    private LocalDateTime endAt;
+    
+    @Column
+    @Enumerated
+    private Priority priority;
+
+    private enum Priority {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
